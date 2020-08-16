dc1fc25dcb5583cc3d08852e53ba0cc0327bd8bd
linxj <line.xj.lin@gmail.com>
2020-08-17 00:28:12 +0800

    relative path

--- scripts/gen_index.sh
+++ scripts/gen_index.sh
@@ -2,5 +2,5 @@
 cd ../articles
 blogTitle="Blog of Line"
 Timefmt="%Y-%m-%d %H:%M:%S"
-tree -D -H "" -C -a -t -r -P "*.html" --noreport  -T "$blogTitle" --timefmt "$Timefmt" | sed 's/<p class="VERSION">/<p hidden>/g' >index.html
+tree -D -H "." -C -a -t -r -P "*.html" --noreport  -T "$blogTitle" --timefmt "$Timefmt" | sed 's/<p class="VERSION">/<p hidden>/g' >index.html
 cd -
