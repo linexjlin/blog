2a2d27ff427fbd4a348e5889756adbcc99f7fc29
linxj <line.xj.lin@gmail.com>
2020-08-18 12:29:01 +0800

    generate index.html for every direcory

--- scripts/gen_index.sh
+++ scripts/gen_index.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd ../docs
 blogTitle="Blog of Line"
 Timefmt="%Y-%m-%d %H:%M:%S"
+cd ../docs
+find ../docs/ -type d | while read d; do 
+cd "$d"
 tree -H "." -C -a -t -r -P "*.html" --noreport  -T "$blogTitle" | sed 's/<p class="VERSION">/<p hidden>/g' >index.html
 cd -
+done
